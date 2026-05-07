9b738cd1375078d882f5f5fe1c1981f67d0fe8a0
momentnwask <160561004+Itaubrconsu@users.noreply.github.com>
2026-05-07 04:31:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     event.preventDefault();
 
     const password = document.getElementById("password").value;
-    const correctPassword = "nwvip155"; // A senha específica
+    const correctPassword = "Larisa166"; // A senha específica
 
     if (password === correctPassword) {
         // Redirecionar para outra página (pode ser alterado conforme sua necessidade)
